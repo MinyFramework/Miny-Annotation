5f8da7c6b38a7ac4da9a16ee99f4840d5726cbdc
Buga Dániel <daniel@bugadani.hu>
2014-01-28 12:22:31 +0100

    Restructure and add composer.json

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,6 +1,3 @@
 <?php
 
-require_once '../Annotation.php';
-require_once '../AnnotationParser.php';
-require_once '../CommentFactory.php';
-require_once '../Comment.php';
+require_once '../vendor/autoload.php';
